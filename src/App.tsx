f45115cbebfb56f0214ddc80b01302f397967bb7
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-07-20 15:18:31 -0400

    Used hash router instead of browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { UserContextProvider } from './context/userContext.tsx'
 import NotFound from './pages/NotFound.tsx';
 import User from './pages/IndividualUser.tsx';
@@ -8,13 +8,13 @@ function App() {
 
   return (
     <UserContextProvider>
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/users/:userId' element={<User />}/>
           <Route path='*' element={<NotFound />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </UserContextProvider>
   )
 }
